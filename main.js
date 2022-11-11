6c6858d5b600451f101217b63b06afcacceac836
void <71181648+dinvoid@users.noreply.github.com>
2022-11-11 10:08:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -31,18 +31,3 @@ $(document).ready(function(){
             	
 });
 
-/*
-         const {}=result.
-     	const {feels_like,humidity,temp}=info.main;
- 		const name=info.name;
- 		const {description,icon,id}=info.weather[0];
-
- 		Object
- 		base: "stations"
- 		clouds: {all: 37}
- 		cod: 200coord: {lon: 124.0671, lat: 10.0307}dt: 1668090440id: 1710908
- 		main: feels_like: 26.03grnd_level: 1010humidity: 82pressure: 1010sea_level: 1010temp: 26.03temp_max: 26.03temp_min: 26.03[[Prototype]]: Object
- 		name: "Inabanga"rain: {1h: 1.78}sys: {type: 1, id: 8187, country: 'PH', sunrise: 1668029792, sunset: 1668071925}timezone: 28800visibility: 10000
- 		weather: [{…}]wind: {speed: 2.92, deg: 72, gust: 3.96}[[Prototype]]: Object
-
-*/
